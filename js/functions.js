0dc8ee4f1b1d330dada0f29a87af47a7fe5210cd
Serhii <serhiy.ilchenko@gmail.com>
2016-10-09 02:22:15 +0300

    home_20161009_0220

--- js/functions.js
+++ js/functions.js
@@ -34,6 +34,109 @@ function placeholderInit(){
 /*placeholder end*/
 
 /**!
+ *  multiselect init
+ * */
+/*! add ui position add class */
+function addPositionClass(position, feedback, obj){
+	removePositionClass(obj);
+	obj.css( position );
+	obj
+		.addClass( feedback.vertical )
+		.addClass( feedback.horizontal );
+}
+
+/*! add ui position remove class */
+function removePositionClass(obj){
+	obj.removeClass('top');
+	obj.removeClass('bottom');
+	obj.removeClass('center');
+	obj.removeClass('left');
+	obj.removeClass('right');
+}
+
+function customSelect(select){
+	if ( select.length ) {
+		selectArray = new Array();
+		select.each(function(selectIndex, selectItem){
+			var placeholderText = $(selectItem).attr('data-placeholder');
+			var flag = true;
+			if ( placeholderText === undefined ) {
+				placeholderText = $(selectItem).find(':selected').html();
+				flag = false;
+			}
+			var classes = $(selectItem).attr('class');
+			selectArray[selectIndex] = $(selectItem).multiselect({
+				header: false,
+				height: 'auto',
+				minWidth: 50,
+				selectedList: 1,
+				classes: classes,
+				multiple: false,
+				noneSelectedText: placeholderText,
+				show: ['fade', 100],
+				hide: ['fade', 100],
+				create: function(event){
+					var select = $(this);
+					var button = $(this).multiselect('getButton');
+					var widget = $(this).multiselect('widget');
+					button.wrapInner('<span class="select-inner"></span>');
+					button.find('.ui-icon').append('<i class="arrow-select"></i>')
+						.siblings('span').addClass('select-text');
+					widget.find('.ui-multiselect-checkboxes li:last')
+						.addClass('last')
+						.siblings().removeClass('last');
+					if ( flag ) {
+						$(selectItem).multiselect('uncheckAll');
+						$(selectItem)
+							.multiselect('widget')
+							.find('.ui-state-active')
+							.removeClass('ui-state-active')
+							.find('input')
+							.removeAttr('checked');
+					}
+				},
+				selectedText: function(number, total, checked){
+					var checkedText = checked[0].title;
+					return checkedText;
+				},
+				position: {
+					my: 'left top',
+					at: 'left bottom',
+					using: function( position, feedback ) {
+						addPositionClass(position, feedback, $(this));
+					}
+				}
+			});
+		});
+		$(window).resize(selectResize);
+	}
+}
+
+function selectResize(){
+	if ( selectArray.length ) {
+		$.each(selectArray, function(i, el){
+			var checked = $(el).multiselect('getChecked');
+			var flag = true;
+			if ( !checked.length ) {
+				flag = false
+			}
+			$(el).multiselect('refresh');
+			if ( !flag ) {
+				$(el).multiselect('uncheckAll');
+				$(el)
+					.multiselect('widget')
+					.find('.ui-state-active')
+					.removeClass('ui-state-active')
+					.find('input')
+					.removeAttr('checked');
+			}
+			$(el).multiselect('close');
+		});
+	}
+}
+/* multiselect init end */
+
+/**!
  * print
  * */
 function printShow() {
@@ -44,9 +147,496 @@ function printShow() {
 }
 /*print end*/
 
+/**!
+ * main navigation
+ * */
+(function ($) {
+	// external js:
+	// 1) TweetMax VERSION: 1.19.0 (widgets.js);
+	// 2) device.js 0.2.7 (widgets.js);
+	// 3) resizeByWidth (resize only width);
+	var MainNavigation = function (settings) {
+		var options = $.extend({
+			mainContainer: 'html',
+			navContainer: null,
+			navMenu: '.nav-list',
+			btnMenu: '.btn-menu',
+			navMenuItem: '.nav-list > li',
+			navMenuAnchor: 'a',
+			navDropMenu: '.js-nav-drop',
+			staggerItems: null,
+			overlayClass: '.nav-overlay',
+			overlayAppend: 'body',
+			overlayAlpha: 0.8,
+			classNoClickDrop: '.no-click', // Класс, при наличии которого дроп не буте открываться по клику
+			classReturn: null,
+			overlayBoolean: false,
+			animationSpeed: 300,
+			animationSpeedOverlay: null,
+			minWidthItem: 100
+		},settings || {});
+
+		var self = this,
+			container = $(options.navContainer),
+			_animateSpeed = options.animationSpeed;
+
+		self.options = options;
+		self.$mainContainer = $(options.mainContainer);            // Основной контейнер дом дерева. по умолчанию <html></html>
+		self.$navMenu = $(options.navMenu);
+		self.$btnMenu = $(options.btnMenu);                        // Кнопка открытия/закрытия меню для моб. верси;
+		self.$navContainer = container;
+		self.$navMenuItem = $(options.navMenuItem, container);     // Пункты навигации;
+		self.$navMenuAnchor = $(options.navMenuAnchor, container); // Элемент, по которому производится событие (клик);
+		self.$navDropMenu = $(options.navDropMenu, container);     // Дроп-меню всех уровней;
+		self.$staggerItems = options.staggerItems || self.$navMenuItem;  //Элементы в стеке, к которым применяется анимация. По умолчанию navMenuItem;
+
+		self._animateSpeed = _animateSpeed;
+		self._classNoClick = options.classNoClickDrop;
+
+		// overlay
+		self._overlayBoolean = options.overlayBoolean;            // Добавить оверлей (по-умолчанию == false). Если не true, то не будет работать по клику вне навигации;
+		self._overlayClass = options.overlayClass;                // Класс оверлея;
+		self.overlayAppend = options.overlayAppend;               // Элемент ДОМ, вконец которого добавится оверлей, по умолчанию <body></body>;
+		self.$overlay = $('<div class="' + self._overlayClass.substring(1) + '"></div>'); // Темплейт оверлея;
+		self._overlayAlpha = options.overlayAlpha;
+		self._animateSpeedOverlay = options.animationSpeedOverlay || _animateSpeed;
+		self._minWidthItem = options.minWidthItem;
+
+		self.desktop = device.desktop();
+
+		self.modifiers = {
+			active: 'active',
+			opened: 'nav-opened',
+			openStart: 'nav-opened-start'
+		};
+
+		self.createOverlay();
+		self.toggleNav();
+		self.clearStyles();
+	};
+
+	MainNavigation.prototype.navIsOpened = false;
+
+	// init tween animation
+	MainNavigation.prototype.overlayTween = new TimelineMax({paused: true});
+
+	// overlay append to "overlayAppend"
+	MainNavigation.prototype.createOverlay = function () {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var $overlay = self.$overlay;
+		$overlay.appendTo(self.overlayAppend);
+
+		TweenMax.set($overlay, {autoAlpha: 0});
+
+		self.overlayTween.to($overlay, self._animateSpeedOverlay / 1000, {autoAlpha: self._overlayAlpha});
+	};
+
+	// show/hide overlay
+	MainNavigation.prototype.showOverlay = function (close) {
+		var self = this;
+		if (!self._overlayBoolean) return false;
+
+		var overlayTween = self.overlayTween;
+
+		if (close === false) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		if (overlayTween.progress() != 0 && !overlayTween.reversed()) {
+			overlayTween.reverse();
+			return false;
+		}
+
+		overlayTween.play();
+	};
+
+	// switch nav
+	MainNavigation.prototype.toggleNav = function () {
+		var self = this,
+			$buttonMenu = self.$btnMenu;
+
+		self.preparationAnimation();
+
+		$buttonMenu.on('click', function (e) {
+			if (self.navIsOpened) {
+				self.closeNav();
+			} else {
+				self.openNav();
+			}
+
+			e.preventDefault();
+		});
+
+		$(document).on('click', self._overlayClass, function () {
+			self.closeNav();
+		});
+	};
+
+	// open nav
+	MainNavigation.prototype.openNav = function() {
+		var self = this,
+			$html = self.$mainContainer,
+			$navContainer = self.$navContainer,
+			$buttonMenu = self.$btnMenu,
+			_animationSpeed = self._animateSpeedOverlay,
+			$staggerItems = self.$staggerItems;
+
+		$buttonMenu.addClass(self.modifiers.active);
+		$html.addClass(self.modifiers.openStart);
+
+		$navContainer.css({
+			'-webkit-transition-duration': '0s',
+			'transition-duration': '0s'
+		});
+
+		var navTween = new TimelineMax();
+
+		navTween
+			.to($navContainer, _animationSpeed / 1000, {
+				yPercent: 0, onComplete: function () {
+					$html.addClass(self.modifiers.opened);
+					TweenMax.staggerTo($staggerItems, 0.3, {autoAlpha:1, scale:1, ease:Cubic.easeInOut}, 0.08);
+				}, ease:Cubic.easeInOut
+			});
+
+		self.showOverlay();
+
+		self.navIsOpened = true;
+	};
+
+	// close nav
+	MainNavigation.prototype.closeNav = function() {
+		var self = this,
+			$html = self.$mainContainer,
+			$navContainer = self.$navContainer,
+			$buttonMenu = self.$btnMenu,
+			_animationSpeed = self._animateSpeedOverlay;
+
+		$html.removeClass(self.modifiers.opened);
+		$html.removeClass(self.modifiers.openStart);
+		$buttonMenu.removeClass(self.modifiers.active);
+
+		self.showOverlay(false);
+
+		TweenMax.to($navContainer, _animationSpeed / 1000, {
+			yPercent: 120, onComplete: function () {
+				self.preparationAnimation();
+			}
+		});
+
+		self.navIsOpened = false;
+	};
+
+	// preparation element before animation
+	MainNavigation.prototype.preparationAnimation = function() {
+		var self = this,
+			$navContainer = self.$navContainer,
+			$staggerItems = self.$staggerItems,
+			$btnMenu = self.$btnMenu;
+
+		if ($btnMenu.is(':visible')) {
+			TweenMax.set($navContainer, {yPercent: 120, onComplete: function () {
+				$navContainer.show(0);
+			}});
+			TweenMax.set($staggerItems, {autoAlpha: 0, scale: 0.8});
+		}
+	};
+
+	// clearing inline styles
+	MainNavigation.prototype.clearStyles = function() {
+		var self = this,
+			$btnMenu = self.$btnMenu,
+			$navContainer = self.$navContainer,
+			$staggerItems = self.$staggerItems;
+
+		//clear on horizontal resize
+		$(window).on('resizeByWidth', function () {
+			if (!$btnMenu.is(':visible')) {
+				$navContainer.attr('style', '');
+				$staggerItems.attr('style', '');
+			} else {
+				self.closeNav();
+			}
+		});
+	};
+
+	window.MainNavigation = MainNavigation;
+
+}(jQuery));
+
+function mainNavigationInit(){
+	var $container = $('.nav');
+	if(!$container.length){ return; }
+	new MainNavigation({
+		navContainer: $container,
+		overlayAppend: '.wrapper',
+		animationSpeed: 300,
+		overlayBoolean: true,
+		overlayAlpha: 0.75
+	});
+}
+/*main navigation end*/
+
+/**
+ * add class on scroll to top
+ * */
+function headerShow(){
+	// external js:
+	// 1) resizeByWidth (resize only width);
+
+	var $page = $('html'),
+		minScrollTop = $('.header').outerHeight();
+
+	var previousScrollTop = $(window).scrollTop();
+	$(window).on('load scroll resizeByWidth', function () {
+		var currentScrollTop = $(window).scrollTop();
+		var showHeaderPanel = currentScrollTop < minScrollTop || currentScrollTop < previousScrollTop;
+
+		$page.toggleClass('header-show', showHeaderPanel);
+
+		previousScrollTop = currentScrollTop;
+	});
+}
+/*add class on scroll to top end*/
+
+/**
+ * add class on scroll to top
+ * */
+function pageIsScrolled(){
+	// external js:
+	// 1) resizeByWidth (resize only width);
+
+	var $page = $('html'),
+		minScrollTop = $('.header').outerHeight();
+
+	$(window).on('load scroll resizeByWidth', function () {
+		var currentScrollTop = $(window).scrollTop();
+		var showHeaderPanel = (currentScrollTop >= minScrollTop);
+
+		$page.toggleClass('page-is-scrolled', showHeaderPanel);
+	});
+}
+/*add class on scroll to top end*/
+
+/**!
+ * sliders
+ * */
+function slidersInit() {
+	//banners slider
+	var $bannerSliders = $('.banners-slider');
+
+	if($bannerSliders.length) {
+		$bannerSliders.each(function() {
+			var $currentSlider = $(this);
+
+			$currentSlider.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true,
+				arrows: false,
+				adaptiveHeight: false,
+				responsive: [
+					{
+						breakpoint: 980,
+						settings: {
+							slidesToShow: 4,
+							slidesToScroll: 4,
+							adaptiveHeight: false
+						}
+					}, {
+						breakpoint: 900,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 3
+						}
+					}, {
+						breakpoint: 640,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					}, {
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
+		});
+	}
+
+	//promo slider
+	var $promoSliders = $('.promo-slider');
+
+	if($promoSliders.length) {
+		$promoSliders.each(function() {
+			var $currentSlider = $(this);
+
+			$currentSlider.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true,
+				arrows: true
+			});
+		});
+	}
+
+	//shares slider
+	var $sharesSliders = $('.shares-slider');
+
+	if($sharesSliders.length) {
+		$sharesSliders.each(function() {
+			var $currentSlider = $(this);
+
+			$currentSlider.slick({
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: false,
+				arrows: true,
+				responsive: [
+					{
+						breakpoint: 1280,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2
+						}
+					},
+					{
+						breakpoint: 640,
+						settings: {
+							autoplay: false,
+							arrows: false,
+							dots: false,
+							draggable: false,
+							swipe: false,
+							touchMove: false
+						}
+					}
+				]
+			});
+		});
+	}
+}
+/*sliders end*/
+
+/**
+ * popup initial
+ * */
+function popupInitial(){
+	$('.popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		tClose: 'Закрыть (Esc)',
+		tLoading: 'Загрузка...',
+
+		fixedContentPos: true,
+		callbacks:{
+			beforeClose: function() {
+				$('.mfp-opened').removeClass('mfp-opened');
+			}
+		}
+	});
+
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+		mainClass: 'mfp-fade',
+		fixedContentPos: true,
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: true,
+		fixedContentPos: true,
+		mainClass: 'mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+}
+/*popup initial end*/
+
+/**!
+ * footer at bottom
+ * */
+function footerBottom(){
+	var $footer = $('.footer');
+	if($footer.length){
+		var $tplSpacer = $('<div />', {
+			class: 'spacer'
+		});
+
+		$('.content').after($tplSpacer.clone());
+		$('.main').after($tplSpacer.clone());
+
+		$(window).on('load resizeByWidth', function () {
+			var footerOuterHeight = $footer.find('.footer-holder').outerHeight();
+			$footer.css({
+				// 'margin-top': -footerOuterHeight
+			});
+
+			$('.spacer').css({
+				'height': footerOuterHeight
+			});
+		})
+	}
+}
+/*footer at bottom end*/
+
+/**!
+ * preloader
+ * */
+function preloadPage(){
+	$('html').addClass('page-loaded');
+}
+/*preloader end*/
+
 /** ready/load/resize document **/
+
+$(window).load(function () {
+	preloadPage();
+});
 
 $(document).ready(function(){
 	placeholderInit();
+	if(DESKTOP){
+		customSelect($('select.cselect'));
+	}
 	printShow();
+	mainNavigationInit();
+	headerShow();
+	pageIsScrolled();
+	slidersInit();
+	popupInitial();
+
+	footerBottom();
 });
