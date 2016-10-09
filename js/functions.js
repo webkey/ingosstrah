6c561801754e949e8f91a44e9321665e7d37d73e
Serhii <serhiy.ilchenko@gmail.com>
2016-10-10 00:37:52 +0300

    home_20161010_0035

--- js/functions.js
+++ js/functions.js
@@ -620,6 +620,119 @@ function preloadPage(){
 }
 /*preloader end*/
 
+/**
+ * multi accordion
+ * */
+(function ($) {
+	var MultiAccordion = function (settings) {
+		var options = $.extend({
+			collapsibleAll: false,
+			animateSpeed: 300,
+			resizeCollapsible: false
+		}, settings || {});
+
+		this.options = options;
+		var container = $(options.accordionContainer);
+		this.$accordionContainer = container; //блок с аккордеоном
+		this.$accordionItem = $(options.accordionItem, container); //непосредственный родитель сворачиваемого элемента
+		this.$accordionEvent = $(options.accordionEvent, container); //элемент, по которому производим клик
+		this.$collapsibleElement = $(options.collapsibleElement); //элемент, который сворачивается/разворачивается
+		this._collapsibleAll = options.collapsibleAll;
+		this._animateSpeed = options.animateSpeed;
+		this.$totalCollapsible = $(options.totalCollapsible);//элемент, по клику на который сворачиваются все аккордены в наборе
+		this._resizeCollapsible = options.resizeCollapsible;//флаг, сворачивание всех открытых аккордеонов при ресайзе
+
+		this.modifiers = {
+			active: 'active',
+			current: 'current'
+		};
+
+		this.bindEvents();
+		this.totalCollapsible();
+		this.totalCollapsibleOnResize();
+
+	};
+
+	MultiAccordion.prototype.totalCollapsible = function () {
+		var self = this;
+		self.$totalCollapsible.on('click', function () {
+			self.$collapsibleElement.slideUp(self._animateSpeed);
+			self.$accordionItem.removeClass(self.modifiers.active);
+			self.$accordionItem.removeClass(self.modifiers.current);
+		})
+	};
+
+	MultiAccordion.prototype.totalCollapsibleOnResize = function () {
+		var self = this;
+		$(window).on('resize', function () {
+			if(self._resizeCollapsible){
+				self.$collapsibleElement.slideUp(self._animateSpeed);
+				self.$accordionItem.removeClass(self.modifiers.active);
+			}
+		});
+	};
+
+	MultiAccordion.prototype.bindEvents = function () {
+		var self = this,
+			modifiers = this.modifiers,
+			animateSpeed = this._animateSpeed,
+			accordionContainer = this.$accordionContainer,
+			anyAccordionItem = this.$accordionItem,
+			collapsibleElement = this.$collapsibleElement;
+
+		self.$accordionEvent.on('click', function (e) {
+			var current = $(this);
+			var currentAccordionItem = current.closest(anyAccordionItem);
+
+			if (!currentAccordionItem.has(collapsibleElement).length){
+				return;
+			}
+
+			e.preventDefault();
+
+			if (current.parent().prop("tagName") != currentAccordionItem.prop("tagName")) {
+				current = current.parent();
+			}
+
+			if (current.siblings(collapsibleElement).is(':visible')){
+				currentAccordionItem.removeClass(modifiers.active).find(collapsibleElement).slideUp(animateSpeed);
+				currentAccordionItem.removeClass(modifiers.current);
+				currentAccordionItem.find(anyAccordionItem).removeClass(modifiers.active).removeClass(modifiers.current);
+				return;
+			}
+
+
+			if (self._collapsibleAll){
+				var siblingContainers = $(accordionContainer).not(current.closest(accordionContainer));
+				siblingContainers.find(collapsibleElement).slideUp(animateSpeed);
+				siblingContainers.find(anyAccordionItem).removeClass(modifiers.active).removeClass(modifiers.current);
+			}
+
+			currentAccordionItem.siblings().removeClass(modifiers.active).find(collapsibleElement).slideUp(animateSpeed);
+			currentAccordionItem.siblings().removeClass(modifiers.current);
+			currentAccordionItem.siblings().find(anyAccordionItem).removeClass(modifiers.active).removeClass(modifiers.current);
+
+			currentAccordionItem.addClass(modifiers.active);
+			current.siblings(collapsibleElement).slideDown(animateSpeed);
+		})
+	};
+
+	window.MultiAccordion = MultiAccordion;
+}(jQuery));
+
+function menuAccordionInit() {
+	if($('.menu__list').length){
+		new MultiAccordion({
+			accordionContainer: '.menu__list',
+			accordionItem: 'li',
+			accordionEvent: 'a',
+			collapsibleElement: '.menu-drop, .menu-sub-drop',
+			animateSpeed: 200
+		});
+	}
+}
+/*multi accordion end*/
+
 /** ready/load/resize document **/
 
 $(window).load(function () {
@@ -637,6 +750,7 @@ $(document).ready(function(){
 	pageIsScrolled();
 	slidersInit();
 	popupInitial();
+	menuAccordionInit();
 
 	footerBottom();
 });
